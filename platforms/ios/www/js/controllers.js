6dafcae6b949ca3b687fe2987b374cacd0961998
Lucas Henrique <lholiveiraa@gmail.com>
2016-04-14 15:43:03 -0300

    Platforms inseridos

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,29 @@
+angular.module('app.controllers', [])
+
+.controller('obrasCtrl', function($scope) {
+
+})
+
+.controller('favoritasCtrl', function($scope) {
+
+})
+
+.controller('contatosCtrl', function($scope) {
+
+})
+
+.controller('combateACorrupcaoCtrl', function($scope) {
+
+})
+
+.controller('nomeDaObraCtrl', function($scope) {
+
+})
+
+.controller('comentariosCtrl', function($scope) {
+
+})
+
+.controller('fotosCtrl', function($scope) {
+
+})
